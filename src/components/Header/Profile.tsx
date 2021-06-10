722cb754fb9a4b43cda87190fe48df011bd8fa16
Thiago Reis <thiagoreis93@gmail.com>
2021-06-09 21:48:16 -0300

    update refactor components

---
+++ src/components/Header/Profile.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
+
+export function Profile() {
+  return (
+    <Flex align="center">
+      <Box mr="4" textAlign="right">
+        <Text>Diego Fernandes</Text>
+        <Text color="gray.300" font="small">
+          diego.schell.f@gmail.com
+        </Text>
+      </Box>
+      <Avatar
+        size="md"
+        name="Diego Fernandes"
+        src="https://github.com/diego3g.png"
+      />
+    </Flex>
+  );
+}
---
+++ src/components/Pagination/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Box, Stack } from "@chakra-ui/react";
+import { PaginationItem } from "./PaginationItem";
+
+export function Pagination() {
+  return (
+    <Stack
+      spacing="6"
+      direction="row"
+      mt="8"
+      justify="space-between"
+      algin="center"
+    >
+      <Box>
+        <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
+      </Box>
+
+      <Stack spacing="2" direction="row">
+        <PaginationItem number={1} isCurrent />
+        <PaginationItem number={2} />
+        <PaginationItem number={3} />
+        <PaginationItem number={4} />
+        <PaginationItem number={5} />
+      </Stack>
+    </Stack>
+  );
+}
